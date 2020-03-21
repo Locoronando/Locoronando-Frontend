b2749048990c4c28239648a8219bf269d8db44f6
Tammo0987 <tammo.steffens@googlemail.com>
2020-03-21 15:57:53 +0100

    Fixed old build errors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="home">
     <Head> </Head>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <b-button v-b-modal.welcomeModal>Launch demo modal</b-button>
     <overlay />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import overlay from '@/components/PlzOverlay.vue'
 import Head from '@/components/Header.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
     Head,
     overlay
   },
