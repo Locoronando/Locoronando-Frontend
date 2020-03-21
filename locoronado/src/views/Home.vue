f6f68bdf5e0d29f850bb3659e89fb481cd0e6848
Oli <oli@Olis-MacBook-Pro.local>
2020-03-21 14:50:57 +0100

    Add PLZ overlay

--- locoronado/src/views/Home.vue
+++ locoronado/src/views/Home.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-button v-b-modal.welcomeModal>Launch demo modal</b-button>
+    <overlay />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import overlay from '@/components/PlzOverlay.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    overlay
+  },
+  mounted () {
+    console.log(this)
+    // this.$refs.welcomeModal.show()
   }
 }
 </script>
