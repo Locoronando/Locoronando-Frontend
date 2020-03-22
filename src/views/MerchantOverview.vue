eab0201bc46c8988a4db1543ef5d67e01c67d2e1
Tammo0987 <tammo.steffens@googlemail.com>
2020-03-22 02:34:47 +0100

    Refactored the overview

--- src/views/MerchantOverview.vue
+++ src/views/MerchantOverview.vue
@@ -1,45 +1,51 @@
 <template>
-  <b-container class="overview">
-    <b-row>
-      <img class="col-12" :src="header" />
+  <b-container>
+    <b-row v-if="this.merchant.header" class="my-4">
+      <img class="col-12" :src="this.merchant.header" />
     </b-row>
-    <b-row class="mt-2">
-      <b-col class="col-sm-7 " >
-        <h3>{{ name }}</h3>
-        <div>{{ description }}</div>
-        <p class="adress mt-1">
-          <span>{{ street }}</span><br />
-          <span>{{ zip }}</span><br />
-          <span>{{ phone }}</span><br />
-        </p>
-      </b-col>
-      <img class="col-sm-5 map" :src="map" />
-    </b-row>
-    <b-row class="mt-2">
 
-      <div class="col-1"></div>
-      <div class="col-10" v-if="contentType === 'application/pdf'">
-        <Pdf
-          :src="products.pdf"
-          :page = "currentPage"
-        ></Pdf>
-        <b-row>
-          <b-col />
-          <button class="col-3 btn btn-outline-success font-weight-bold" @click="changePage(-1)"> - </button>
-          <b-col />
-          <button class="col-3 btn btn-outline-success font-weight-bold" @click="changePage(1)"> + </button>
-          <b-col />
+    <b-row class="my-4">
+      <b-col class="col-sm-7 parent">
+        <h3 class="mb-4">{{ this.merchant.name }}</h3>
+        <div class="flex">
+          <span>{{ this.merchant.description }}</span>
+
+          <div>
+            <p class="mt-1">
+              <span>{{ this.merchant.address.street }}</span>
+              <br />
+              <span>{{ this.merchant.address.city }}</span>
+              <br />
+              <span>{{ this.merchant.phone }}</span>
+              <br />
+            </p>
+          </div>
+        </div>
+      </b-col>
+
+      <img class="col-sm-5" :src="this.merchant.map" />
+    </b-row>
+
+    <b-row class="mt-2 mb-4">
+
+      <b-col v-if="contentType === 'application/pdf'">
+        <Pdf :src="products.pdf" :page="page" @num-pages="maxPages = $event"></Pdf>
+        <b-row align-h="around">
+          <button class="col-3 btn btn-outline-success font-weight-bold" @click="changePage(-1)">-</button>
+          <span>{{ this.page }}</span>
+          <button class="col-3 btn btn-outline-success font-weight-bold" @click="changePage(1)">+</button>
         </b-row>
-      </div>
-      <div class="col-1"></div>
+      </b-col>
 
       <div v-if="contentType === 'application/json'">
         <b-card-group deck>
           <div id="cards" class="col-sm-3" v-for="product in products" :key="product.id">
-            <b-card class="mb-4"
-            @click="addToCart(product.id)"
-            :title="product.name"
-            :sub-title="product.prize + ' €'" >
+            <b-card
+              class="mb-4"
+              @click="addToCart(product.id)"
+              :title="product.name"
+              :sub-title="product.prize + ' €'"
+            >
               <b-card-text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</b-card-text>
             </b-card>
           </div>
@@ -50,36 +56,45 @@
         <img :src="products" />
       </div>
     </b-row>
+
     <button class="request-btn btn btn-success">Anfrage {{ items > 0 ? items : '' }}</button>
   </b-container>
 </template>
 
 <script>
 import Pdf from 'vue-pdf'
+
 export default {
-  name: 'Home',
+  name: 'Overview',
+  components: {
+    Pdf
+  },
   data () {
     return {
-      header: 'https://www.kurhaus-salinenparc.de/wp-content/uploads/2017/09/header-restaurant-bar.jpg',
-      name: 'San Marco',
-      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ',
-      street: 'Samplestreet 27',
-      zip: '39221 Berlin',
-      phone: '+4981281283',
-      map: 'https://images-na.ssl-images-amazon.com/images/I/81AH5Xe0XXL._SX425_.jpg',
-      products: null,
-      contentType: null,
-      pageCount: 0,
-      currentPage: 1,
+      id: this.$route.params.id,
+      merchant: {
+        name: 'San Marco',
+        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ',
+        header: 'https://www.kurhaus-salinenparc.de/wp-content/uploads/2017/09/header-restaurant-bar.jpg',
+        address: {
+          street: 'Samplestreet 42',
+          city: '39221 Berlin'
+        },
+        phone: '+4981281283',
+        map: 'https://images-na.ssl-images-amazon.com/images/I/81AH5Xe0XXL._SX425_.jpg'
+      },
+      products: undefined,
+      contentType: undefined,
+      page: 1,
+      maxPages: 0,
       cart: {},
       items: 0
     }
   },
   methods: {
     changePage (i) {
-      if (this.currentPage > 0 || this.currentPage < this.pageCount) {
-        this.currentPage = this.currentPage + i
-        console.log(this.currentPage)
+      if (this.page + i > 0 && this.page + i <= this.maxPages) {
+        this.page += i
       }
     },
     loadMerchant () {
@@ -92,40 +107,42 @@ export default {
         this.cart[id] = 1
       }
       this.items++
-      this.$nextTick(function () {
-        console.log('Update UI')
-      })
     },
     loadProducts () {
       // this.contentType = 'application/pdf'
-      // this.products = { pdf: 'https://pizzeria-ristorante-san-marco.de/dokumente/speisekarte.pdf', pages: 4 }
+      // this.products = { pdf: 'https://pizzeria-ristorante-san-marco.de/dokumente/speisekarte.pdf' }
       this.contentType = 'application/json'
-      this.products = [{ id: 1, name: 'Test1', prize: '128' }, { id: 2, name: 'Test2', prize: '128' }, { id: 3, name: 'Test3', prize: '128' }, { id: 4, name: 'Test4', prize: '128' }, { id: 5, name: 'Test5', prize: '128' }]
-      this.$nextTick(function () {
-        console.log('Update UI')
-      })
+      this.products = [
+        { id: 1, name: 'Test 1', prize: '128' },
+        { id: 2, name: 'Test 2', prize: '128' },
+        { id: 3, name: 'Test 3', prize: '128' },
+        { id: 4, name: 'Test 4', prize: '128' },
+        { id: 5, name: 'Test 5', prize: '128' }
+      ]
     }
-  },
-  components: {
-    Pdf
-  },
-  props: {
-    id: String
   },
   beforeMount () {
     this.loadMerchant()
     this.loadProducts()
   }
 }
-
 </script>
 
 <style scoped>
-.map{
-  height: 100%;
+.parent {
+  display: flex;
+  flex-direction: column;
 }
 
-.request-btn{
+.flex {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+}
+
+.request-btn {
   position: fixed;
   bottom: 20px;
   right: 20px;
